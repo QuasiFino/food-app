b23ee49fdc42e6583ea0f1f144d8a37dae8bdcda
wink-aajlak <aajlak.wink@gmail.com>
2021-11-11 14:26:59 +0530

    footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import './App.css';
 import Navbar from './componets/Navbar';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Footer from './componets/Footer';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar />
+        <Routes>
+          <Route path="/" element={<Home/>} />
+        </Routes>
+        <Footer />
       </Router>
     </div>
   );
